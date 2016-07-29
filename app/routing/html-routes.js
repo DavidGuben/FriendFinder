2d32fee5dd194093a33746574ee3639334dc1e39
David Guben <david.guben@rutgers.edu>
2016-07-29 13:30:15 -0400

    Added syntax for questions

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -3,12 +3,5 @@ app.use('/', function(req, res){
 });
 
 app.get('/survery', function(req, res) {
-    var html = '<h1> Friend Finder Survey </h1>';
-        html += '<select id ="q1">';
-          html += '<input>option 1</input>';
-          html += '<input>option 2</input>';
-          html += '<input>option 3</input>';
-          html += '<input>option 4</input>';
-          html += '<input>option 5</input>';
-        html += '</select>';
+  res.sendFile(path.join(__dirname, 'home.html'));
 });
