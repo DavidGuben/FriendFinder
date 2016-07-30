04445169abcccb524ad7c0ca2468ac3789e28eb3
David Guben <david.guben@rutgers.edu>
2016-07-29 23:53:33 -0400

    added submit button for survey

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -3,5 +3,5 @@ app.use('/', function(req, res){
 });
 
 app.get('/survery', function(req, res) {
-  res.sendFile(path.join(__dirname, 'home.html'));
+  res.sendFile(path.join(__dirname, 'survey.html'));
 });
