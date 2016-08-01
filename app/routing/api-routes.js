ddceefd8da21ee8d3b96b83edd213b03b5fc984c
David Guben <david.guben@rutgers.edu>
2016-07-31 23:13:17 -0400

    attempting to send ajax to friends.js

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -21,6 +21,8 @@ app.post('/data/friends', function(req, res) {
 	// * Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both 5-3 and 3-5 as 2, and so on.
 	// * The closest match will be the user with the least amount of difference.
 
+  
+
 
 });
 };
