99fecc5206dfa9b09a6389da03d17043d5725bbe
David Guben <david.guben@rutgers.edu>
2016-08-01 17:28:58 -0400

    added array and transfered names and info

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,5 +1,11 @@
 var path = require('path');
 
+var friends = [
+  {
+  name: "Dummy",
+  scores: [3,2,4,5,1,2,4,5,3,2]
+  },
+]
 
 module.exports = function (app) {
 // This will be used to display a JSON of all possible friends.
@@ -10,19 +16,11 @@ app.get('/data/friends', function(req, res) {
 // This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 app.post('/data/friends', function(req, res) {
 
-	// * Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
-	//* With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the totalDifference.
-	//*
-	//	* Example:
-	//	*
-	//		* User 1: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-	//		* User 2: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
-	//		* Total Difference: 2 + 1 + 2 = 5
-	// * Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both 5-3 and 3-5 as 2, and so on.
-	// * The closest match will be the user with the least amount of difference.
-
-  
-
+  var newFriend = req.body;
+  console.log(req.body);
+  friends.push(newFriend);
+  res.json(newFriend);
+  console.log(friends);
 
 });
 };
